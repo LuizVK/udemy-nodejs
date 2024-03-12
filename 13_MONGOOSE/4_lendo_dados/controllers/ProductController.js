505e5acbbc3f2518721dc6122f597d56a67dab90
Luiz Vitor <luizv.karpinski@unochapeco.edu.br>
2024-03-12 12:16:21 -0300

    Capítulo de Mongoose

---
+++ 13_MONGOOSE/4_lendo_dados/controllers/ProductController.js
@@ -0,0 +1,62 @@
+import Product from "../models/Product.js"
+
+export default class ProductController {
+    static async showProducts(req, res) {
+        const products = await Product.find().lean()
+
+        res.render('products/all', { products })
+    }
+
+    static createProduct(req, res) {
+        res.render('products/create')
+    }
+
+    static async createProductSave(req, res) {
+        const { name, image, price, description } = req.body
+
+        const product = new Product({ name, image, price, description })
+
+        await product.save()
+
+        res.redirect('/products')
+    }
+
+    // static async getProduct(req, res) {
+    //     const id = req.params.id
+
+    //     console.log('1 - ' + id)
+
+    //     const product = await Product.getProductById(id.trim())
+
+    //     if(!product)
+    //         return res.sendStatus(404)
+
+    //     res.render('products/product', { product })
+    // }
+
+    // static async removeProduct(req, res) {
+    //     const { id } = req.params
+
+    //     await Product.removeProductById(id.trim())
+
+    //     res.redirect('/products')
+    // }
+
+    // static async editProduct(req, res) {
+    //     const id = req.params.id
+
+    //     const product = await Product.getProductById(id)
+
+    //     res.render('products/edit', { product })
+    // }
+
+    // static async editProductPost(req, res) {
+    //     const { id, name, image, price, description } = req.body
+
+    //     const product = new Product(name, image, price, description)
+
+    //     await product.updateProduct(id)
+
+    //     res.redirect('/products')
+    // }
+}
---
+++ 13_MONGOOSE/6_editando_dado/routes/productsRoutes.js
@@ -0,0 +1,14 @@
+import express from "express"
+import ProductController from "../controllers/ProductController.js"
+
+const router = express.Router()
+
+router.get('/create', ProductController.createProduct)
+router.post('/create', ProductController.createProductSave)
+// router.post('/remove/:id', ProductController.removeProduct)
+router.get('/edit/:id', ProductController.editProduct)
+// router.post('/edit/', ProductController.editProductPost)
+router.get('/:id', ProductController.getProduct)
+router.get('/', ProductController.showProducts)
+
+export default router
